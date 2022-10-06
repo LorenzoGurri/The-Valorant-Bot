18715d41c46e5e717de5848df99964e21077f321
AGreenFruit <danielchentheavocado@gmail.com>
2022-10-05 20:24:26 -0400

    Added 2 more bot commands
    Added both a dad joke command as well as send a gif if asked for !tvb Sova Ascent B

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 bot = discord.Client(intents = discord.Intents.all())
 
+command = "!tbv"
+
 # EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
 @bot.event
 async def on_ready():
@@ -26,20 +28,28 @@ async def on_ready():
 		guild_count = guild_count + 1
 
 	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
-	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+	print("TheValorantBot is in " + str(guild_count) + " guilds.")
 
 # EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
 @bot.event
 async def on_message(message):
-	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	# Make sure it doesn't respond to itself in it's own response
+	if message.author == bot.user:
+		return
 
-    if message.content == "hello":
-        # SENDS BACK A MESSAGE TO THE CHANNEL.
-        print("In hello")
-        await message.channel.send("hey dirtbag")
-    print("hello")
-    print("message contnet: ")
-    print(message.content)
+	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	if message.content == "hello":
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		print("In hello")
+		await message.channel.send("hey dirtbag")
+	elif message.content.startswith("I'm"):
+		msg = message.content.split()[1]
+		await message.channel.send("Hi " + msg + ", I'm dad!")
+	elif message.content == "!tvb Sova Ascent B":
+		await message.channel.send(file=discord.File('lineups/sova_ascent_b.gif'))
+	print("hello")
+	print("message contnet: ")
+	print(message.content)
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
 bot.run(DISCORD_TOKEN)
